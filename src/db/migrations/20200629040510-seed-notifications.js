201446542cc1f1f930ab859361040ca582044705
dhruvinparikh93 <imsilent93@gmail.com>
2020-07-30 16:37:24 -0400

    correct migrations

--- src/db/migrations/20200629040510-seed-notifications.js
+++ src/db/migrations/20200629040510-seed-notifications.js
@@ -9,6 +9,15 @@ module.exports = {
         short_description: 'totaly supply for Dai ',
         long_description: 'Dai is a stable, decentralized and unbiased currency.',
         d_app_uuid: '0d4d4c2c-f403-4046-b07e-606a60af9f7f'
+      },
+      {
+        uuid: '00000002-2c32-4564-8d54-e00d4001b744',
+        created_at: new Date(),
+        updated_at: new Date(),
+        name: 'WETH <> WBTC',
+        short_description: 'WETH per WBTC',
+        long_description: 'WETH <> WBTC rate for Uniswap\'s exchange',
+        d_app_uuid: '4c4c510c-f12c-4c62-b824-c511490f3a80'
       }
     ]);
   },
--- src/db/seeders/20200629040510-seed-notifications.js
+++ src/db/seeders/20200629040510-seed-notifications.js
@@ -5,28 +5,19 @@ module.exports = {
         uuid: '00000001-2c32-4564-8d54-e00d4001b744',
         created_at: new Date(),
         updated_at: new Date(),
-        name: 'Name 1',
-        short_description: 'Short Description 1',
-        long_description: 'Long Long Long  Long  Long  Long  Long  Long  Long  Long  Long  Long  Long  Long  Long  Long  Long  Long Description 1',
-        d_app_uuid: '4c4c510c-f12c-4c62-b824-c511490f3a80'
+        name: 'totalDAISupply',
+        short_description: 'totaly supply for Dai ',
+        long_description: 'Dai is a stable, decentralized and unbiased currency.',
+        d_app_uuid: '0d4d4c2c-f403-4046-b07e-606a60af9f7f'
       },
       {
         uuid: '00000002-2c32-4564-8d54-e00d4001b744',
         created_at: new Date(),
         updated_at: new Date(),
-        name: 'Name 2',
-        short_description: 'Short Description 2',
-        long_description: 'Long Long Long  Long  Long  Long  Long  Long  Long  Long  Long  Long  Long  Long  Long  Long  Long  Long Description 2',
+        name: 'WETH <> WBTC',
+        short_description: 'WETH per WBTC',
+        long_description: 'WETH <> WBTC rate for Uniswap\'s exchange',
         d_app_uuid: '4c4c510c-f12c-4c62-b824-c511490f3a80'
-      },
-      {
-        uuid: '00000003-2c32-4564-8d54-e00d4001b744',
-        created_at: new Date(),
-        updated_at: new Date(),
-        name: 'Name 3',
-        short_description: 'Short Description 3',
-        long_description: 'Long Long Long  Long  Long  Long  Long  Long  Long  Long  Long  Long  Long  Long  Long  Long  Long  Long Description 3',
-        d_app_uuid: '0d4d4c2c-f403-4046-b07e-606a60af9f7f'
       }
     ]);
   },
