42a9be9fc55a27599124807736444eb057fa831d
Ezadkiel Marbella <emarbella@apigarage.com>
2020-08-20 22:03:51 -0400

    update config and rename  seeders

--- src/services/web3/index.js
+++ src/services/web3/index.js
@@ -3,13 +3,17 @@ const config = require('../../config')
 
 const web3 = new Web3(
   new Web3.providers.HttpProvider(
-    `https://mainnet.infura.io/v3/${config.web3.getInfuraKey()}`
+    `https://mainnet.infura.io/v3/${config.web3.getInfuraProjectID()}`
   )
 );
 
 async function getBlockNumber() {
-  const blockNumber = await web3.eth.getBlockNumber();
-  return blockNumber;
+  try {
+    const blockNumber = await web3.eth.getBlockNumber();
+    return blockNumber;      
+  } catch (error) {
+    return Error("Issue Getting Block Number");
+  }
 }
 
 module.exports = { web3, getBlockNumber };
